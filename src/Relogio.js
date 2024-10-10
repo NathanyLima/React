c614c68e665d55bb4db69552059bf928fc6369d6
Nathany <129187491+NathanyLima@users.noreply.github.com>
2024-10-10 12:22:27 -0300

    Add files via upload

---
+++ src/Relogio.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+ 
+function Relogio() {
+    const [horarioAtual, setHorarioAtual] = useState("");
+ 
+    useEffect(() => {
+        const atualizarRelogio = () => {
+            const data = new Date();
+            const horas = String(data.getHours()).padStart(2, '0');
+            const minutos = String(data.getMinutes()).padStart(2, '0');
+            const segundos = String(data.getSeconds()).padStart(2, '0');
+            setHorarioAtual(`${horas}:${minutos}:${segundos}`);
+        };
+ 
+        atualizarRelogio();
+        const intervalo = setInterval(atualizarRelogio, 1000);
+ 
+        return () => clearInterval(intervalo);
+    }, []);
+ 
+    return (
+      <>
+        <h1>Meu Relógio</h1>
+        <h2 id="relogio">{horarioAtual}</h2>
+        </>
+    );
+}
+ 
+export default Relogio;
+ 
+ 
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Relogio from './Relogio';
+import Letreiro from './Letreiro';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Relogio />
+    <Letreiro/>
+</>
+);
+
