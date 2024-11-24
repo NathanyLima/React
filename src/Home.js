c6740fb6f35ac63eb1d8467b6f9cb7aa3e81484d
Nathany <129187491+NathanyLima@users.noreply.github.com>
2024-11-24 01:57:02 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,10 @@ const Home = () => {
                 <ul>
                     <li><Link to="/atv01">Atividade 1</Link></li>
                     <li><Link to="/atv02">Atividade 2</Link></li>
+                    <li><Link to="/atv03">Atividade 3</Link></li>
+                    <li><Link to="/atv04">Atividade 4</Link></li>
+
+
                 </ul>
             </nav>
         </div>
