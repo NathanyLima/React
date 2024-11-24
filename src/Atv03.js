f33a16c73579a485f42c214fa94175ced5b29151
Nathany <129187491+NathanyLima@users.noreply.github.com>
2024-11-24 16:18:07 -0300

    Update Atv03.js

--- src/Atv03.js
+++ src/Atv03.js
@@ -5,7 +5,7 @@ import atividade3 from "./atividade3";
 const Atv03 = () => {
     return (
         <div>
-            <h1>Atividade 03</h1>
+            <h1> Atividade 03</h1>
             <atividade3 />  
             <Link to="/">Voltar para a Home</Link>
         </div>
