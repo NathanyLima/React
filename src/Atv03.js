49c7854454ab0f3e7a8e29f285d7a75041d4710e
Nathany <129187491+NathanyLima@users.noreply.github.com>
2024-11-24 16:44:47 -0300

    Update Atv03.js

--- src/Atv03.js
+++ src/Atv03.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Atividade3 from "./Atividade3"; 
+import Atv03 from "./Componentes/Atv03"; 
 
 const Atv03 = () => {
-    return (
-        <div>
-            <h1>Atividade 03</h1>
-            <Atividade3/>  {
-            <Link to="/">Voltar para a Home</Link>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Atividade 03</h1>
+      <Atv0 />
+      <Link to="/">Voltar para a Home</Link>
+    </div>
+  );
+};
 
 export default Atv03;
